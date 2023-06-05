e73b53ded95f6020ccad3ded898c039a4f2c5688
BigBlueDraco <Dafarlog@gmail.com>
2023-06-05 13:26:00 +0300

    Changed functions that return unique values ​​from a string

--- src/UserData.js
+++ src/UserData.js
@@ -39,56 +39,13 @@ export default class UserData {
   getUniqeWords() {
     const array = this.getWords();
 
-    const uniqueElements = [];
-    array.forEach((item) => {
-      const indexOf = uniqueElements.indexOf(item);
-      if (indexOf !== -1) {
-        uniqueElements.splice(indexOf, 1);
-        return;
-      }
-      uniqueElements.push(item);
-    });
+    const uniqueElements = [...new Set(array)];
     return uniqueElements || [];
-
-    // Цей ваш штучний інтелект таке запропонував не знаю що краще
-    // const array = ["A", "aaa", "aaa", "123", "123", "4"];
-
-    // // Підрахунок кількості входжень кожного елемента
-    // const count = {};
-    // array.forEach((item) => {
-    //   count[item] = (count[item] || 0) + 1;
-    // });
-
-    // // Сортування масиву за кількістю входжень, а потім за значенням
-    // const sortedArray = array.sort((a, b) => {
-    //   if (count[a] !== count[b]) {
-    //     return count[a] - count[b];
-    //   } else {
-    //     return a.localeCompare(b);
-    //   }
-    // });
-
-    // // Вибір лише унікальних елементів
-    // const uniqueElements = sortedArray.filter((item, index) => {
-    //   return index === sortedArray.indexOf(item);
-    // });
-
-    // console.log(uniqueElements);
   }
 
   getUnique() {
     const array = this.getAll();
-
-    const uniqueElements = [];
-
-    array.forEach((item) => {
-      const indexOf = uniqueElements.indexOf(item);
-      if (indexOf !== -1) {
-        uniqueElements.splice(indexOf, 1);
-        return;
-      }
-      uniqueElements.push(item);
-    });
+    const uniqueElements = [...new Set(array)];
     return uniqueElements || [];
   }
 }
